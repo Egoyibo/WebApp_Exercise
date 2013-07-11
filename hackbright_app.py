7fc62eb14f73a5930f6d9b52bb2ebe6055f4209b
ChrUbuntu User <user@chromebox-002.(none)>
2013-07-10 17:36:14 -0700

    done through number 5

---
+++ hackbright_app.py
@@ -0,0 +1,132 @@
+#Connecting SQL to Python ###Writing Up Our Database to WebApp
+#7_02_13 ### 7_10_13
+#Joy Ding and Katherine Hennes ###Kate Odell and Jessica Mong
+
+import sqlite3
+
+DB = None
+CONN = None
+
+HELP_MSGS = {
+    "student": "Command format is: student, <student name>",
+    "project": "project, <project title"
+}
+
+def get_student_by_github(github):
+    query = """SELECT first_name, last_name, github FROM Students2 WHERE github = ?"""
+    DB.execute(query, (github,)) # Must be tuple even if only one value is subbed
+    row = DB.fetchone()
+    return row
+    #Modified code to return only row (07/10/13)
+
+    # if row == None:
+    #     return "Student does not exist!"
+    # else: 
+    #     #changed print to return
+    #     return """\
+    #     Student: %s %s
+    #     Github account: %s"""%(row[0], row[1], row[2])
+
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects2 WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+    Project: %s
+    Description %s
+    Max Grade: %d""" % (row[0], row[1], row[2]) 
+
+def get_grades_by_project(title):
+    query = """SELECT first_name, last_name, github, grade 
+    FROM GradesView2
+    WHERE project_title = ?"""
+    DB.execute(query, (title,))
+    rows = DB.fetchall()
+    return rows
+    # removed to sync up for webapp
+    # print "OK"
+    # print """\
+    # Project: %s
+    # Grade: %d""" %(row[0], row[1])
+
+def update_grade(grade, project_title, student_github):
+    query = """UPDATE Grades2 SET grade=? WHERE student_github=? and project_title=?"""
+    DB.execute(query, (grade, student_github, project_title))
+    CONN.commit()
+    print "Successfully added %s's grade for %s: %d " %(student_github, project_title, int(grade)) 
+
+def get_grades_by_student(first_name, last_name):
+    #query = """SELECT S.first_name, S.last_name, P.title, G.grade
+    query = """SELECT P.title, G.grade
+    FROM Grades2 G
+    JOIN Students2 S ON (S.github = G.student_github)
+    JOIN Projects2 P ON (G.project_title = P.title)
+    WHERE S.first_name = ? and S.last_name = ?"""
+
+    DB.execute(query, (first_name, last_name))
+    rows=DB.fetchall()
+    return rows
+    # removed to sync up for webapp
+    # print "\tStudent: %s %s" % (first_name, last_name)
+    # for row in rows:
+    #     print """\
+    #     \tProject: %s
+    #     \t\tGrade: %d""" % (row[2], row[3])
+
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students2 values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" %(first_name, last_name)
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects2 values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s %s" % (title, description)
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("my_database.db") # DB connection executes queries
+    DB = CONN.cursor()
+
+def main():
+    connect_to_db()
+    command = None
+    while command != "quit":
+        input_string = raw_input("HBA Database> ")
+        tokens = input_string.split(", ")
+        command = tokens[0]
+        args = tokens[1:]
+
+        try:
+            if command == "student":
+                # if len(tokens) == 2: #add error message for wrong number of arguments
+                get_student_by_github(*args)
+                # else:
+                # print "Looking for a particular student? Go ahead and type 'student, <github name>'" 
+            elif command == "new_student":
+                make_new_student(*args)
+            elif command == "project":
+                get_project_by_title(*args)
+            elif command == "new_project":
+                make_new_project(*args)
+            elif command == "grade":
+                get_grade_by_project(*args)
+            elif command == "new_grade":
+                update_grade(*args)
+            elif command == "get_grades":
+                get_grades_by_student(*args)
+            else:
+                print "Please type a real command!"
+        except TypeError, e:
+            print HELP_MSGS[command]
+        except NameError, e:
+            pass
+        except:
+            pass
+
+    CONN.close()
+
+if __name__ == "__main__":
+    main()
---
+++ webapp.py
@@ -0,0 +1,58 @@
+import hackbright_app
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_github():
+    return render_template("get_github.html")
+
+@app.route("/add_student")
+def add_student():
+    return render_template("add_student.html")
+
+@app.route("/add_project")
+def add_project():
+    return render_template("add_project.html")
+
+@app.route("/student")
+def get_student():
+    hackbright_app.connect_to_db()
+    student_github = request.args.get("github")
+    row = hackbright_app.get_student_by_github(student_github)
+    grades = hackbright_app.get_grades_by_student(row[0], row[1])
+
+    return render_template("student_info.html", first_name = row[0],
+                                            last_name = row[1],
+                                            github = row[2],
+                                            grades = grades)
+
+@app.route("/new_student")
+def make_student():
+    hackbright_app.connect_to_db()
+    first = request.args.get("first")
+    last = request.args.get("last")
+    github = request.args.get("github")
+    hackbright_app.make_new_student(first, last, github)
+    return redirect("/student?github="+github)
+
+@app.route("/project")
+def get_project():
+    hackbright_app.connect_to_db()
+    project_name = request.args.get("name")
+    grades = hackbright_app.get_grades_by_project(project_name)
+
+    return render_template("project_info.html", name = project_name,
+                                                grades = grades)
+
+@app.route("/new_project")
+def make_project():
+    hackbright_app.connect_to_db()
+    ptitle = request.args.get("ptitle")
+    description = request.args.get("description")
+    max_score = request.args.get("max_score")
+    hackbright_app.make_new_project(ptitle, description, max_score)
+    return redirect("/project?name="+ptitle)
+
+if __name__ == "__main__":
+    app.run(debug=True)
